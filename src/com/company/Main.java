c8e22c30c80ba50a45bd4a098cccd9d94dac26d9
Thomas Tunc <thomastunc@hotmail.com>
2018-12-14 10:32:27 +0100

    Added new features for the DEPQ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import com.company.depq.DEPQ;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	new Main().run();
+    }
+
+    private void run() {
+        DEPQ<String> strings = new DEPQ<>();
+
+        System.out.println(strings.isEmpty());
+        strings.add("Geld"); //Hoogste prioriteit
+        strings.add("Gelukkig"); //2 na hoogste prioriteit
+        strings.add("Liefde"); //3 na hoogste prioriteit
+        strings.add("Dood"); //Laagste prioriteit
+        System.out.println(strings.size());
+        System.out.println(strings.getHigh());
+        System.out.println(strings.getLow());
+        System.out.println(strings.isEmpty());
+        strings.add("We gaan naar huis");
+        strings.removeHigh();
+        strings.removeHigh();
+        strings.removeHigh();
+        strings.removeHigh();
+        System.out.println(strings.getHigh());
     }
 }
